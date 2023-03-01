049aad8f0320eddaf1560dc8583cd35f110c75c9
sannchan <xp.gateway.2009@gmail.com>
2023-03-01 20:01:11 +0900

    first

---
+++ chi.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import time
+import tkinter as tk
+from tkinter import ttk
+
+class app:
+    def __init__(self):
+        pass
+    def disp(self):
+        
+        print("disp")
+    def clock(self):
+        print("clock")
+    def memo(self):
+        print("memo")
+        self.clock()
+apprun = app()
+apprun.disp()
+apprun.clock()
+apprun.memo()
+
