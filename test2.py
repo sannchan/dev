ab0782d193f9e390368566bb4ae55539f6b1b4ef
sannchan <xp.gateway.2009@gmail.com>
2023-03-10 17:34:22 +0900

    deda

--- test2.py
+++ test2.py
@@ -1,19 +1,32 @@
-from PyQt6.QtWidgets import QApplication, QWidget, QLabel
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QFileDialog
 from PyQt6.QtGui import QPixmap
 
-app = QApplication([])
-window = QWidget()
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
 
-# 画像の読み込み
-pixmap = QPixmap("Image.png")
+        # 背景画像を表示するためのラベルを作成
+        self.background_label = QLabel(self)
+        self.background_label.setGeometry(0, 0, 453, 244)
 
-# 幅453、高さ244のQPixmapオブジェクトを作成
-pixmap = pixmap.scaled(453, 244)
+        # ファイル選択ボタンを作成
+        self.select_file_button = QPushButton("ファイルを選択", self)
+        self.select_file_button.setGeometry(10, 10, 100, 30)
+        self.select_file_button.clicked.connect(self.select_file)
 
-# 背景画像を表示するためのラベルを作成
-background_label = QLabel(parent=window)
-background_label.setPixmap(pixmap)
-background_label.setGeometry(0, 0, pixmap.width(), pixmap.height())
+    def select_file(self):
+        # ファイル選択ダイアログを表示
+        file_path, _ = QFileDialog.getOpenFileName(
+            self, "ファイルを選択", "", "画像ファイル (*.png *.jpg *.jpeg)")
 
-window.show()
-app.exec()
+        if file_path:
+            # 画像を読み込み、背景に設定
+            pixmap = QPixmap(file_path)
+            self.background_label.setPixmap(pixmap)
+            self.background_label.setScaledContents(True)
+
+if __name__ == '__main__':
+    app = QApplication([])
+    window = MainWindow()
+    window.show()
+    app.exec()
